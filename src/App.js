877858c3acc1a2a6083a40e5f3d4c639f78f67c6
fh95-tech <fakih.hussein995@gmail.com>
2024-12-29 22:40:57 +0200

    updated style sheets

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import SignIn from './pages/SignIn/SignIn';
 import SignUp from './pages/SignUp/SignUp';
+import Footer from './components/Footer/Footer'
+import Navbar from './components/Navbar/Navbar'
 // import "./ui/styles/base.css"
 
 function App() {
   return (
     <div className='app'>
-      <SignUp/>
-     
+      <Navbar/>
+     <Footer/>
     </div>
   );
 }
