7976c77fad02d00ea9bc36c20b8dbcebdab6ac4d
fh95-tech <fakih.hussein995@gmail.com>
2024-12-29 20:19:10 +0200

    added responsiveness @ 768px for signup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import SignIn from './pages/SignIn/SignIn';
+import SignUp from './pages/SignUp/SignUp';
 // import "./ui/styles/base.css"
 
 function App() {
   return (
     <div className='app'>
-      <SignIn />
+      <SignUp/>
      
     </div>
   );
