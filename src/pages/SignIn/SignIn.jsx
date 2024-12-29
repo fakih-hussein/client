28a728ffa5866581a4dd88735a1d900ad2fe0528
fh95-tech <fakih.hussein995@gmail.com>
2024-12-29 17:26:40 +0200

    added google sign in button

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,19 +1,23 @@
 import React from "react";
 import logo from "./../../assets/images/logo.png"
+import Glogo from "./../../assets/images/google-logo.png"
 import "./../../ui/styles/base.css"
 
-const SignIn=()=>{
-    return(
+const SignIn = () => {
+    return (
         <div className="main-div flex column center">
             <div className="logo"><img src={logo} alt="logo" /></div>
             <p className="bold">Sign In</p>
             <p>Sign in to save your trip plans and access them on any device.</p>
             <p>New to Plan Mate AI? <a href="#signup" className="bold">Sign Up</a></p>
-            <input type="email" placeholder="Email"/>
-            <input type="password" placeholder="Password"/>
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
             <button type="submit">Continue</button>
+            <div>
+                <button><img src={Glogo}/>Continue with google</button>
+            </div>
         </div>
-    )   
+    )
 }
 
 export default SignIn
