88b6cdf3caf0a0486d5db09966dc22b8dbe078ef
fh95-tech <fakih.hussein995@gmail.com>
2024-12-29 22:57:45 +0200

    implemented redux for state management

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import SignIn from './pages/SignIn/SignIn';
 import SignUp from './pages/SignUp/SignUp';
 import Footer from './components/Footer/Footer'
 import Navbar from './components/Navbar/Navbar'
-// import "./ui/styles/base.css"
 
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 function App() {
   return (
-    <div className='app'>
-      <Navbar/>
-     <Footer/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navbar/>}/>
+        <Route path="/signin" element={<SignIn/>}/>
+      </Routes>
+    </Router>
+
   );
 }
 
 export default App;
-  
