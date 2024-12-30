0d373abe54704003a71fcf840022a631fdd3e562
fh95-tech <fakih.hussein995@gmail.com>
2024-12-30 11:25:56 +0200

    configured redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,2 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import modalReducer from ""
+import modalReducer from "./modalSlice";
+
+const store = configureStore({
+    reducer: {
+        modal: modalReducer,
+        },
+});
+
+export default store;
