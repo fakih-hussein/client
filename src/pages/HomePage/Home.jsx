1504d85f0f76806c77d8eef0026ac2ff78a8b472
fh95-tech <fakih.hussein995@gmail.com>
2025-01-02 20:02:27 +0200

    commited and designed third section of landing page

--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -6,63 +6,72 @@ import Footer from "../../components/footer/Footer";
 import SignIn from "../SignIn/SignIn";
 import loc from "./../../assets/images/loc-white.png"
 import map from "./../../assets/images/map.webp"
-import food from "./../../assets/images/food.webp"
+import destinations from "./../../assets/images/destinations.webp"
 
 const Home = () => {
     return (
         <div>
             <NavBar />
-            <div className="home flex column">
-                <div className="landing flex column center font-fam">
-                    <h1 className="bold">Your next journey optimized</h1>
-                    <h3>Build, personalize, and optimize your itineraries
-                        with our free AI trip planner. Designed for vacations,
-                        workations, and everyday adventures.</h3>
-                    <button className="new-trip flex row center">
-                        <img src={loc} alt="loc"></img>
-                        <span>Create New Trip</span>
-                    </button>
+            <div className="flex column center">
+                <div className="home flex column">
+                    <div className="landing flex column center font-fam">
+                        <h1 className="bold">Your next journey optimized</h1>
+                        <h3>Build, personalize, and optimize your itineraries
+                            with our free AI trip planner. Designed for vacations,
+                            workations, and everyday adventures.</h3>
+                        <button className="new-trip flex row center">
+                            <img src={loc} alt="loc"></img>
+                            Create New Trip
+                        </button>
+                    </div>
+
                 </div>
 
-            </div>
-
-            <div className="second flex column center font-fam">
-                <h2>Your AI-Powered trip</h2>
-                <div className="flex row">
-                    <div className="flex column">
-                        <div className="flex column">
-                            <h3 className="bold">The most optimal</h3>
-                            <span className="text">
-                                Craft your perfect itinerary with
-                                Plan Mate AI. Our advanced algorithms take into
-                                account your selected explore-sights, dining, and
-                                lodging preferences to create the optimal travel plan
-                                tailored just for you.
-                            </span>
-                        </div>
-                        <div className="img-second">
-                            <img src={map} alt="map"></img>
-                        </div>
+                <div className="third flex column center">
+                    <div className="flex column center">
+                    <h2>The only tool you’ll ever need!</h2>
+                    <p>Say goodbye to the stress of planning 
+                        and hello to personalized recommendations, 
+                        efficient itineraries, and seamless 
+                        dining experiences.</p>
                     </div>
-                    <div className="flex column">
-                        <div className="flex column">
-                            <div className="img-second">
-                                <img src={food} alt="map"></img>
+                    
+                    <div className="containers flex row">
+                        <div className="container flex column">
+                            <div className="flex row">
+                                <img src="" alt></img>
+                                <h2 className="bold">Optimal Route Planning</h2>
                             </div>
-                            <h3 className="bold">Get Inspired</h3>
-                            <span className="text">
-                                Extract valuable travel insights from Instagram
-                                reels and TikToks, explore the mentioned
-                                explore-sights, and effortlessly include them in
-                                your own adventure with Plan Mate AI.
-                            </span>
+                            <p>Our AI algorithms analyze your 
+                                preferences to craft the most 
+                                efficient route, saving you time 
+                                and effort.</p>
+                        </div>
+                        <div className="container flex column">
+                            <div className="flex row">
+                                <img src="" alt></img>
+                                <h2 className="bold">Personalize Your Adventure</h2>
+                            </div>
+                            <p>Shape your journey by freely adding, 
+                                editing, or deleting activities from 
+                                your itinerary.</p>
+                        </div>
+                        <div className="container flex column">
+                            <div className="flex row">
+                                <img src="" alt></img>
+                                <h2 className="bold">Local Cuisine Recommendations</h2>
+                            </div>
+                            <p>Discover local cuisines and hidden 
+                                gems recommended by our AI, tailored
+                                 to your taste buds.</p>
                         </div>
 
                     </div>
 
                 </div>
-
             </div>
+
+            
             <Footer />
 
         </div>
