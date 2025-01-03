071c30d8e40f7788e354448bc61ec2fbec45798c
fh95-tech <fakih.hussein995@gmail.com>
2025-01-03 12:34:30 +0200

    handled the linking to signup form

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -5,22 +5,16 @@ import Glogo from "./../../assets/images/google-logo.png"
 import "./../../ui/styles/base.css"
 import SignUp from "../SignUp/SignUp";
 
-import { useDispatch, useSelector } from "react-redux";
-import { openModal, closeModal } from "../../redux/modalSlice";
+import { useDispatch } from "react-redux";
+import { showSignUp } from "../../redux/modalSlice";
 
 const SignIn = () => {
     const dispatch = useDispatch();
-    const isModalOpen = useSelector((state) => state.modal.isModalOpen);
 
     const handleSignUpClick = () => {
-        dispatch(openModal());
+        dispatch(showSignUp());
     }
 
-    const handleCloseModal = (event) => {
-        if (event.target.classList.contains("modal-overlay")) {
-            dispatch(closeModal());
-        }
-    }
     return (
 
         <div className="form flex column center">
@@ -30,7 +24,7 @@ const SignIn = () => {
                 <p className="title bold">Sign In</p>
                 <p className="subtitle">Sign in to save your trip plans and access them on any device.</p>
             </div>
-            <p className="link">New to Plan Mate AI? <a href="#signup" className="bold" onClick={handleSignUpClick}>Sign Up</a></p>
+            <p className="link">New to Plan Mate AI? <a className="bold" onClick={handleSignUpClick}>Sign Up</a></p>
             <div className="input flex column space-between">
                 <input className="font-fam" type="email" placeholder="Email" />
                 <input className="font-fam" type="password" placeholder="Password" />
@@ -40,13 +34,7 @@ const SignIn = () => {
             <div className="google-div flex center ">
                 <button className="flex row center"><img src={Glogo} />Continue with google</button>
             </div>
-            {isModalOpen && (
-                <div className="modal-overlaye" onClick={handleCloseModal}>
-                    <div className="modal-content">
-                        <SignUp />
-                    </div>
-                </div>
-            )}
+            
         </div>
 
 
