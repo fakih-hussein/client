df2eebd3d0f56afeb960f4177460ecca93e3faf8
fh95-tech <fakih.hussein995@gmail.com>
2025-01-06 19:56:20 +0200

    added submit button handler

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -21,7 +21,17 @@ const SignUp = () => {
 
     })
 
-    
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await axios.post('http://localhost:8000/api/signup', formData);
+            console.log(response.data.message);
+        }catch(error){
+            console.error(error);
+        }
+
+    }
     return (
 
         <div className="form flex column center">
