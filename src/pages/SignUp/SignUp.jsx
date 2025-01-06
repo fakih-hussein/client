97d24cdec66d09cc9f5fd15fe2a31e64094c9f2a
fh95-tech <fakih.hussein995@gmail.com>
2025-01-06 20:02:43 +0200

    added handlechange event handler

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -14,6 +14,13 @@ const SignUp = () => {
         dispatch(showSignIn());
     }
 
+    const handleChange =(e)=>{
+        setFormData({
+            ...formData,
+            [e.target.name]:e.target.value,
+        });
+    }
+    
     const [formData, setFormData] = useState({
         email: "",
         password: "",
@@ -27,33 +34,33 @@ const SignUp = () => {
         try {
             const response = await axios.post('http://localhost:8000/api/signup', formData);
             console.log(response.data.message);
-        }catch(error){
+        } catch (error) {
             console.error(error);
         }
 
     }
     return (
+        <form onSubmit={handleSubmit}>
+            <div className="form flex column center">
 
-        <div className="form flex column center">
+                <div className="flex column center">
+                    <div className="logo"><img src={logo} alt="logo" /></div>
+                    <p className="title bold">Sign Up</p>
+                    <p className="subtitle">Signing up with Plan Mate AI is fast and free.</p>
+                </div>
+                <p className="link">Already have an account? <a onClick={handleSignInClick} className="bold">Sign In</a></p>
+                <div className="input flex column space-between">
+                    <input className="font-fam" type="email" placeholder="Email" />
+                    <input className="font-fam" type="password" placeholder="Password" />
+                    <input className="font-fam" type="password" placeholder="Confirm Password" />
+                    <button className="font-fam" type="submit">Continue</button>
+                </div>
 
-            <div className="flex column center">
-                <div className="logo"><img src={logo} alt="logo" /></div>
-                <p className="title bold">Sign Up</p>
-                <p className="subtitle">Signing up with Plan Mate AI is fast and free.</p>
+                <div className="google-div flex center ">
+                    <button className="flex row center"><img src={Glogo} />Sign Up with google</button>
+                </div>
             </div>
-            <p className="link">Already have an account? <a onClick={handleSignInClick} className="bold">Sign In</a></p>
-            <div className="input flex column space-between">
-                <input className="font-fam" type="email" placeholder="Email" />
-                <input className="font-fam" type="password" placeholder="Password" />
-                <input className="font-fam" type="password" placeholder="Confirm Password" />
-                <button className="font-fam" type="submit">Continue</button>
-            </div>
-
-            <div className="google-div flex center ">
-                <button className="flex row center"><img src={Glogo} />Sign Up with google</button>
-            </div>
-        </div>
-
+        </form>
     )
 }
 
